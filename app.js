f53577b5e59710a4457bcdd5c5e1d1342eccae46
DKosmos <davidakosmos@gmail.com>
2017-05-11 21:34:42 -0700

    created YNQuestion function and call for my questions

--- app.js
+++ app.js
@@ -5,90 +5,36 @@ var correctAnswers = 0;
 var userName = prompt('What\'s your name?');
 alert('Hello ' + userName + ', I\'m going to ask you a few questions. The first five are yes/no questions.');
 
-var petsArray = [];
-do{
-  var pets = prompt('Does David have any pets?').toUpperCase();
+var questionArray = ['Does David have any pets?','Does David have a cat?','Does David walk to class each day?','Does David walk his dog during his lunch hour?','Is David still working while he takes Code 201?'];
+var correctAnswerArray = [['Y','N','Y','Y','N'],['YES','NO','YES','YES','NO']];
+var correctResponseArray = ['You\'re right! He does have a pet.','You\'re right! David has a dog, not a cat.','Right you are, but he only lives two blocks away so it\'s not that impressive.','Well he usually does. Sometime his dog goes to daycare.','You\'re right, he took the month off from work to learn code!'];
+var incorrectResponseArray = ['Actually, he does have a pet.','Wrong, David has a dog.','Actually he does walk to class, but he only lives two blocks away.','He actually walks his dog most days during the lunch hour. Occasionally his dog goes to daycare for the whole day.','There\'s no way David could work while taking classes from 9am to 6pm.'];
+var userAnswerArray = [];
 
-  if (pets === 'YES' || pets === 'Y'){
-    alert('You\'re right! He does have a pet.');
-    ++correctAnswers;
-  } else if (pets === 'NO' || pets === 'N'){
-    alert('Actually, he does have a pet.');
-  } else {
-    alert('That\'s not a yes or no answer! Try again.');
-    petsArray.push(pets);
+function askYNQuestion(qArr, corrArr, corrResp, incoResp){
+  var userAnsReturn = [];
+  for (var i=0; i<qArr.length; i++){
+    do{
+      var userAnswer = prompt(qArr[i]).toUpperCase();
+
+      if (userAnswer !== 'Y' && userAnswer !== 'YES' && userAnswer !== 'N' && userAnswer !== 'NO'){
+        alert('Please respond only with a yes or no.');
+      } else if (userAnswer === corrArr[0][i] || userAnswer === corrArr[1][i]){
+        alert(corrResp[i]);
+        correctAnswers++;
+      } else {
+        alert(incoResp[i]);
+      }
+    } while (userAnswer !== 'YES' && userAnswer !== 'Y' && userAnswer !== 'NO' && userAnswer !== 'N');
+    console.log('User Answer: ', userAnswer);
+    userAnsReturn.push(userAnswer);
   }
-} while (pets !== 'YES' && pets !== 'Y' && pets !== 'NO' && pets !== 'N');
-console.log('Pets Guess: ', pets);
-console.log('Not Y/N: ', petsArray);
+  alert('You answered ' + correctAnswers + ' out of ' + qArr.length + ' correctly!');
+  return userAnsReturn;
+}
 
-var petTypeArray = [];
-do{
-  var petType = prompt('Does David have a cat?').toUpperCase();
-
-  if(petType === 'YES' || petType === 'Y'){
-    alert('Wrong, David has a dog.');
-  } else if (petType === 'NO' || petType === 'N'){
-    alert('You\'re right! David has a dog, not a cat.');
-    ++correctAnswers;
-  } else {
-    alert('Just answer yes or no, you\'re better than this.');
-    petTypeArray.push(petType);
-  }
-} while(petType !== 'YES' && petType !== 'Y' && petType !== 'NO' && petType !== 'N');
-console.log('David has cat: ', petType);
-console.log('Not Y/N: ', petTypeArray);
-
-var homeArray = [];
-do{
-  var home = prompt('Does David walk to class each day?').toUpperCase();
-
-  if (home === 'YES' || home === 'Y'){
-    alert('Right you are, but he only lives two blocks away so it\'s not that impressive.');
-    ++correctAnswers;
-  } else if(home === 'NO' || home === 'N'){
-    alert('Actually he does walk to class, but he only lives two blocks away.');
-  } else {
-    alert('By now you should know these are yes/no questions. Let\'s try this again.');
-    homeArray.push(home);
-  }
-} while(home !== 'YES' && home !== 'Y' && home !== 'NO' && home !== 'N');
-console.log('Walks to class: ', home);
-console.log('Not Y/N: ', homeArray);
-
-var lunchArray = [];
-do{
-  var lunch = prompt('Does David walk his dog during his lunch hour?').toUpperCase();
-
-  if (lunch === 'YES' || lunch === 'Y'){
-    alert('Well he usually does. Sometime his dog goes to daycare.');
-    ++correctAnswers;
-  } else if (lunch === 'NO' || lunch === 'N'){
-    alert('He actually walks his dog most days during the lunch hour. Occasionally his dog goes to daycare for the whole day.');
-  } else {
-    alert('Just yes or no please.')
-    lunchArray.push(lunch);
-  }
-} while (lunch !== 'YES' && lunch !== 'Y' && lunch !== 'NO' && lunch !== 'N');
-console.log('Walks dog at lunch: ', lunch);
-console.log('Not Y/N: ', lunchArray);
-
-var workArray = [];
-do{
-  var work = prompt('Is David still working while he takes Code 201?').toUpperCase();
-
-  if (work === 'YES' || work === 'Y'){
-    alert('There\'s no way David could work while taking classes from 9am to 6pm.');
-  } else if (work === 'NO' || work === 'N'){
-    alert('You\'re right, he took the month off from work to learn code!');
-    ++correctAnswers;
-  } else {
-    alert('This is the fifth question, you should have it figured out by now.');
-    workArray.push(work);
-  }
-} while (work !== 'YES' && work !== 'Y' && work !== 'NO' && work !== 'N');
-console.log('David working now: ', work);
-console.log('Not Y/N: ', workArray);
+var hopeThisWorks = askYNQuestion(questionArray, correctAnswerArray, correctResponseArray, incorrectResponseArray);
+console.log(hopeThisWorks.join(', '));
 
 var guessesArray = [];
 var numberOfPetsActual = 6;
