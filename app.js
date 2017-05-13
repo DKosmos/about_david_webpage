b95d7d72fd5b0f6a4b333ace6a79728f3df4c671
DKosmos <davidakosmos@gmail.com>
2017-05-13 14:31:02 -0700

    added multiQ function and askallQs function, works but correct answers don't

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 var correctAnswers = 0;
 
 var userName = prompt('What\'s your name?');
@@ -10,9 +10,10 @@ var correctAnswerArray = [['Y','N','Y','Y','N'],['YES','NO','YES','YES','NO']];
 var correctResponseArray = ['You\'re right! He does have a pet.','You\'re right! David has a dog, not a cat.','Right you are, but he only lives two blocks away so it\'s not that impressive.','Well he usually does. Sometime his dog goes to daycare.','You\'re right, he took the month off from work to learn code!'];
 var incorrectResponseArray = ['Actually, he does have a pet.','Wrong, David has a dog.','Actually he does walk to class, but he only lives two blocks away.','He actually walks his dog most days during the lunch hour. Occasionally his dog goes to daycare for the whole day.','There\'s no way David could work while taking classes from 9am to 6pm.'];
 var userAnswerArray = [];
-
+*/
 function askYNQuestion(qArr, corrArr, corrResp, incoResp){
   var userAnsReturn = [];
+  var correctAnswers = 0;
   for (var i=0; i<qArr.length; i++){
     do{
       var userAnswer = prompt(qArr[i]).toUpperCase();
@@ -29,15 +30,16 @@ function askYNQuestion(qArr, corrArr, corrResp, incoResp){
     console.log('User Answer: ', userAnswer);
     userAnsReturn.push(userAnswer);
   }
-  alert('You answered ' + correctAnswers + ' out of ' + qArr.length + ' correctly!');
-  return userAnsReturn;
+  console.log(correctAnswers);
+  return [userAnsReturn, correctAnswers];
 }
-
+/*
 var hopeThisWorks = askYNQuestion(questionArray, correctAnswerArray, correctResponseArray, incorrectResponseArray);
 console.log(hopeThisWorks.join(', '));
-
+*/
 function askNumQuestion (askNum, corrNum, corrNumResponse, attemptsNum){
   var ansNumReturn = [];
+  var correctAnswers = 0;
   var attemptsRemaining = attemptsNum;
   for (var a=0; a<attemptsNum; a++){
     var userAnsNum = parseInt(prompt(askNum));
@@ -59,31 +61,67 @@ function askNumQuestion (askNum, corrNum, corrNumResponse, attemptsNum){
   return [ansNumReturn, correctAnswers];
 }
 
-function askQuestions(ask, correct, correctResponse, incorrectResponse, amountOfAttempts){
+function askMultiAnsQuestion (askMulti, corrMulti, corrRespMulti, incorrRespMulti, attemptsMulti){
+  var ansMultiReturn = [];
   var correctAnswers = 0;
-  var answerArray = [];
-  for (var i=0; i<ask.length; i++){
-    if (typeof correct[i] === 'number'){
-      answerArray.push(askNumQuestion(ask, correct, correctResponse, amountOfAttempts));
-    } else if (typeof correct[i] === 'object'){
-      if (typeof correct[i][0] === 'Y' || typeof correct[i][0] === 'YES' || typeof correct[i][0] === 'N' || typeof correct[i][0] === 'NO'){
-        answerArray.push(askYNQuestion(ask[i], correct[i], correctResponse[i], incorrectResponse[i]));
-      } else {
-        //ask multi-answer question
+  var attemptsLeftMulti = attemptsMulti;
+  for (var i=0; i<attemptsMulti; i++){
+    var userAnsMulti = prompt(askMulti).toUpperCase();
+    ansMultiReturn.push(userAnsMulti);
+    attemptsLeftMulti--;
+    for (var j=0; j<corrMulti.length; j++){
+      if (userAnsMulti === corrMulti[j].toUpperCase()){
+        alert(corrRespMulti);
+        correctAnswers++;
+        return [ansMultiReturn, correctAnswers];
       }
     }
+    if (attemptsLeftMulti === 0){
+      alert('Sorry, that was your last guess.')
+    } else {
+      alert('Nope, try again');
+    }
   }
-  return [correctAnswers, answerArray];
+  alert(incorrRespMulti);
+  console.log(correctAnswers);
+  return [ansMultiReturn, correctAnswers];
+}
+
+function askQuestions(ask, correct, correctResponse, incorrectResponse, amountOfAttempts){
+  var correctFinalAnswers = 0;
+  var answerArray = [];
+  for (var tick=0; tick<ask.length; tick++){
+    if (typeof correct[tick] === 'number'){
+      console.log('Number');
+      var answer = askNumQuestion(ask[tick], correct[tick], correctResponse[tick], amountOfAttempts[tick]);
+      correctFinalAnswers =+ answer[1];
+      answerArray.push(answer[0]);
+    } else if (typeof correct[tick][0] === 'object'){
+      console.log('YN');
+      var answer = askYNQuestion(ask[tick], correct[tick], correctResponse[tick], incorrectResponse[tick]);
+      correctFinalAnswers =+ answer[1];
+      answerArray.push(answer[0]);
+    } else if (typeof correct[tick][0] === 'string'){
+      console.log('Multi');
+      var answer = askMultiAnsQuestion(ask[tick], correct[tick], correctResponse[tick], incorrectResponse[tick], amountOfAttempts[tick]);
+      correctFinalAnswers =+ answer[1];
+      answerArray.push(answer[0]);
+    }
+  }
+  return [correctFinalAnswers, answerArray];
 }
 
 var citiesBaseball = ['New York', 'Chicago', 'Detroit', 'St. Louis', 'San Fransisco', 'Milwaukee', 'Baltimore', 'Boston'];
 
-var questionArray = ['How many household pets has David had in his lifetime?', 'Besides Seattle, name a city in which David has gone to a MLB game.'];
-var correctAnswerArray = [6, ['New York', 'Chicago', 'Detroit', 'St. Louis', 'San Fransisco', 'Milwaukee', 'Baltimore', 'Boston']];
-var correctResponseArray = ['You\'re right! He\'s had two dogs and four cats over the years. But three of the cats belonged to his sister.', 'You\'re right! He\'s had two dogs and four cats over the years. But three of the cats belonged to his sister.', 'You\'re right! He\'s been to games in ' + citiesBaseball.join(', ') + '.'];
-var incorrectResponseArray = ['Sorry, that\'s not it.', 'Nope.'];
-var numberofAttempts = [6, 4];
+var questionArray = [['Does David have any pets?','Does David have a cat?','Does David walk to class each day?','Does David walk his dog during his lunch hour?','Is David still working while he takes Code 201?'], 'How many household pets has David had in his lifetime?', 'Besides Seattle, name a city in which David has gone to a MLB game.'];
+var correctAnswerArray = [[['Y','N','Y','Y','N'],['YES','NO','YES','YES','NO']],6, ['New York', 'Chicago', 'Detroit', 'St. Louis', 'San Fransisco', 'Milwaukee', 'Baltimore', 'Boston']];
+var correctResponseArray = [['You\'re right! He does have a pet.','You\'re right! David has a dog, not a cat.','Right you are, but he only lives two blocks away so it\'s not that impressive.','Well he usually does. Sometime his dog goes to daycare.','You\'re right, he took the month off from work to learn code!'], 'You\'re right! He\'s had two dogs and four cats over the years. But three of the cats belonged to his sister.', 'You\'re right! He\'s been to games in ' + citiesBaseball.join(', ') + '.'];
+var incorrectResponseArray = [['Actually, he does have a pet.','Wrong, David has a dog.','Actually he does walk to class, but he only lives two blocks away.','He actually walks his dog most days during the lunch hour. Occasionally his dog goes to daycare for the whole day.','There\'s no way David could work while taking classes from 9am to 6pm.'], 'Sorry, that\'s not it.', 'Nope.'];
+var numberofAttempts = [1, 6, 4];
 var userAnswerArray = [];
+
+var finalAnswerArray = askQuestions(questionArray, correctAnswerArray, correctResponseArray, incorrectResponseArray, numberofAttempts);
+console.log(finalAnswerArray);
 
 /*
 var guessesArray = [];
